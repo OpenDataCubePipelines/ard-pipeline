c7a14c1718aeebee6b91ecb4bbacd7989b235417
Ben Davies <ben.davies@ga.gov.au>
2025-06-03 14:31:01 +1000

    Add clip negative relative humidity to zero option for MODTRAN.

--- wagl/atmos.py
+++ wagl/atmos.py
@@ -2,6 +2,8 @@
 TODO: Module for atmospheric calculations.
 """
 
+import numpy as np
+
 
 def kelvin_2_celcius(kelvin):
     """A small utility function for converting degrees Kelvin to
@@ -10,9 +12,12 @@ def kelvin_2_celcius(kelvin):
     return kelvin - 273.15
 
 
-def relative_humdity(surface_temp, dewpoint_temp, kelvin=True):
-    """Calculates relative humidity given a surface temperature and
-    dewpoint temperature.
+def relative_humdity(surface_temp, dewpoint_temp, kelvin=True, clip_negative=True):
+    """
+    Calculates relative humidity given a surface temperature & dewpoint temperature.
+
+    MODTRAN doesn't handle negative RH values, thus the default behaviour is to
+    clip negative values to zero to sanitise calculation.
     """
     if kelvin:
         surf_t = kelvin_2_celcius(surface_temp)
@@ -22,5 +27,8 @@ def relative_humdity(surface_temp, dewpoint_temp, kelvin=True):
         dew_t = dewpoint_temp
 
     rh = 100 * ((112.0 - 0.1 * surf_t + dew_t) / (112.0 + 0.9 * surf_t)) ** 8
+
+    if clip_negative:
+        rh = np.where(rh < 0.0, 0.0, rh)
 
     return rh
